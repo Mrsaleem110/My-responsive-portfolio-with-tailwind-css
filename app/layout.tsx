8ab39c6a6674aad32fbc6e82517492a2d1a3da7d
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-10-27 13:09:22 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/components/footer/page";
 import Header from "@/components/header/page";
